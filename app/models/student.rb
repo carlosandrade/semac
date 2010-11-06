2773436397821b93a73415d05ad594481dad35f1
Carlos Andrade <carlosviansi@gmail.com>
2010-11-06 17:29:02 -0300

    Adicionada verificação de unicidade de nome, teste passou

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,9 @@
 class Student < ActiveRecord::Base
 
+  #Não permite que estudantes sejam cadastrados sem nome
   validates_presence_of :name
+
+  #Garante a unicidade de nome por estudante na base de dados
+  validates_uniqueness_of :name
 
 end
