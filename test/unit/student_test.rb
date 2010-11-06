def8fe019c3307c9eeab5fb5dce3afaca73e7165
Carlos Andrade <carlosviansi@gmail.com>
2010-11-06 17:27:02 -0300

    Criado teste de verificação de nome de estudante unico na base de dados

--- test/unit/student_test.rb
+++ test/unit/student_test.rb
@@ -3,16 +3,23 @@ require 'test_helper'
 class StudentTest < ActiveSupport::TestCase
 
   def setup
-    @student = Student.new(:name => "Jose Roberto", :email => "roberto@null.com", :registration => 20101255)
+    @jose_roberto = Student.new(:name => "Jose Roberto", :email => "roberto@null.com", :registration => 20101255)
+    @jose_roberto_almeida = Student.new(:name => "Jose Roberto", :email => "roberto@null.com", :registration => 2923132)
   end
 
   def teardown
-    @student = nil
+    @jose_roberto = nil
+    @jose_roberto_almeida = nil
   end
 
   test "should have a name" do
-    @student.name = nil
-    assert !@student.save, "Saved a student without a name"
+    @jose_roberto.name = nil
+    assert !@jose_roberto.save, "Saved a student without a name"
+  end
+
+  test "name should be unique" do
+    @jose_roberto.save
+    assert !@jose_roberto_almeida.save, "Saved the same name on database, name is not unique"
   end
 
 end
