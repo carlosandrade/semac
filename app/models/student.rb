668a406227e08ab973fbcdd6d467888318ac921b
Carlos Andrade <carlosviansi@gmail.com>
2010-11-06 17:52:55 -0300

    Adicionando testes e verificações de campo obrigatorio, unicidade e limite de 100 chars para email

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,12 +1,25 @@
 class Student < ActiveRecord::Base
 
-  #Não permite que estudantes sejam cadastrados sem nome
+#Name validations
+
+  #Does not allow student names to be empyt
   validates_presence_of :name
 
-  #Garante a unicidade de nome por estudante na base de dados
+  #Student names must be unique
   validates_uniqueness_of :name
 
-  #Verifica se um estudante com nome acima de cem caracteres foi salvo
+  #Student names should not exceed 100 chars
   validates_length_of :name, :maximum=>100
+
+#Email validations
+
+  #Does not allow student email to be empyt
+  validates_presence_of :email
+
+  #Student emails must be unique
+  validates_uniqueness_of :email
+
+  #Student emails should not exceed 100 chars
+  validates_length_of :email, :maximum=>100
 
 end
--- test/unit/student_test.rb
+++ test/unit/student_test.rb
@@ -3,15 +3,21 @@ require 'test_helper'
 class StudentTest < ActiveSupport::TestCase
 
   def setup
+    
+    #Instance variables for names and email tests
     @jose_roberto = Student.new(:name => "Jose Roberto", :email => "roberto@null.com", :registration => 20101255)
     @jose_roberto_almeida = Student.new(:name => "Jose Roberto", :email => "roberto@null.com", :registration => 2923132)
    
   end
 
   def teardown
+    #Teardown for names and email tests
     @jose_roberto = nil
     @jose_roberto_almeida = nil
+
   end
+
+#Name tests
 
   test "should have a name" do
     @jose_roberto.name = nil
@@ -27,8 +33,27 @@ class StudentTest < ActiveSupport::TestCase
    @big_name_student = Student.new(:name => " aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", :email => "bigname@null.com", :registration => 1232351)
 
    assert !@big_name_student.save, "Student with a name over 100 chars has been saved"
-
-
   end
+
+#Email tests
+  
+   test "should have an email" do
+    @jose_roberto.email = nil
+    assert !@jose_roberto.save, "Saved a student without an email"
+  end
+
+  test "email should be unique" do
+    @jose_roberto.save
+    assert !@jose_roberto_almeida.save, "Saved the same email on database, email is not unique"
+  end
+
+  test "student email should not exceed 100" do
+   @big_email_student = Student.new(:name => "Lucas", :email => " aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@whatever.com", :registration => 1232351)
+
+   assert !@big_email_student.save, "Student with an email over 100 chars has been saved"
+  end
+
+
+
 
 end
