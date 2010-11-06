a64003f47c7d15727874bd9cff032a443f8e6ad3
Carlos Andrade <carlosviansi@gmail.com>
2010-11-06 17:14:19 -0300

    Teste que verifica ausencia de nome

--- test/unit/student_test.rb
+++ test/unit/student_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    @student = Student.new(:name => "Jose Roberto", :email => "roberto@null.com", :registration => 20101255)
   end
+
+  def teardown
+    @student = nil
+  end
+
+  test "should have a name" do
+    @student.name = nil
+    assert !@student.save, "Saved a student without a name"
+  end
+
 end
