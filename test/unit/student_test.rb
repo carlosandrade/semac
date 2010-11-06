59ab9b5035c7c5a1f43a07be8cc0b8454ff0a3d7
Carlos Andrade <carlosviansi@gmail.com>
2010-11-06 17:37:27 -0300

    Teste para verificar se um estudante com mais de 100 caracteres foi salvo

--- test/unit/student_test.rb
+++ test/unit/student_test.rb
@@ -5,6 +5,7 @@ class StudentTest < ActiveSupport::TestCase
   def setup
     @jose_roberto = Student.new(:name => "Jose Roberto", :email => "roberto@null.com", :registration => 20101255)
     @jose_roberto_almeida = Student.new(:name => "Jose Roberto", :email => "roberto@null.com", :registration => 2923132)
+   
   end
 
   def teardown
@@ -20,6 +21,14 @@ class StudentTest < ActiveSupport::TestCase
   test "name should be unique" do
     @jose_roberto.save
     assert !@jose_roberto_almeida.save, "Saved the same name on database, name is not unique"
+  end
+
+  test "student name should not exceed 100" do
+   @big_name_student = Student.new(:name => " aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", :email => "bigname@null.com", :registration => 1232351)
+
+   assert !@big_name_student.save, "Student with a name over 100 chars has been saved"
+
+
   end
 
 end
