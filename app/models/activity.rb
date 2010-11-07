1aa48b5a04d5f15dd87d108f9d5e037daadc34f3
Carlos Andrade <carlosviansi@gmail.com>
2010-11-06 22:30:57 -0300

    Adicionada verificação de especificação obrigatoria para atividade, passou no teste

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,9 @@ class Activity < ActiveRecord::Base
   
   #Does not allow student registration to be empty
   validates_presence_of :title
+
+#Specification validations
+  
+  #Does not allow specification registration to be empyt
+  validates_presence_of :specification
 end
